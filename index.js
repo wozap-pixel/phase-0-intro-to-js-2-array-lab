02d6125315f0d19451f08b03766e8a41ae59a174
peter <mutisyapeter336@gmail.com>
2024-10-22 07:28:24 +0300

    modified a file

--- index.js
+++ index.js
@@ -1,1 +1,52 @@
 // Write your solution here!
+// index.js
+
+// Initial array of cats
+const cats = ["Milo", "Otis", "Garfield"];
+
+// Destructive Methods
+function destructivelyAppendCat(name) {
+  cats.push(name);
+}
+
+function destructivelyPrependCat(name) {
+  cats.unshift(name);
+}
+
+function destructivelyRemoveLastCat() {
+  cats.pop();
+}
+
+function destructivelyRemoveFirstCat() {
+  cats.shift();
+}
+
+// Non-destructive Methods
+function appendCat(name) {
+  return [...cats, name];
+}
+
+function prependCat(name) {
+  return [name, ...cats];
+}
+
+function removeLastCat() {
+  return cats.slice(0, cats.length - 1);
+}
+
+function removeFirstCat() {
+  return cats.slice(1);
+}
+
+// Export the functions if needed for testing
+module.exports = {
+  cats,
+  destructivelyAppendCat,
+  destructivelyPrependCat,
+  destructivelyRemoveLastCat,
+  destructivelyRemoveFirstCat,
+  appendCat,
+  prependCat,
+  removeLastCat,
+  removeFirstCat,
+};
